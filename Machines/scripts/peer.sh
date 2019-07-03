f8ac7523bc3436ff299d06c98a2ed33126c7a577
Teodor Dominte <teodor.dominte@info.uaic.ro>
2019-07-03 16:07:06 +0300

    Added vagrant related files

---
+++ Machines/scripts/peer.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+curl -sS https://bootstrap.pypa.io/get-pip.py | sudo python3
+pip3 install jsonpickle
+pip3 install bitstring
+pip3 install pycryptodome
+pip3 install Flask
+pip3 install flask-cors
